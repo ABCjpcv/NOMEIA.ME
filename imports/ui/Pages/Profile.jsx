4ac0327884e9bdcacce58434c413fe0e984c5579
Andre Bras Correia <andre.bras-correia@siemens.com>
2022-05-09 22:45:18 +0100

    trying to add sidebar menu

---
+++ imports/ui/Pages/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Profile = () => {
+    return <div>
+        <div>
+            
+        <div id="profile_div">
+
+        </div>
+            THIS IS profile PAGE
+        </div>
+        
+        
+        
+    </div>
+}
