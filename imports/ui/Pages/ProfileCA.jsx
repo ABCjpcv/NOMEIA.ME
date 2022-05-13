e4338e845f46b00cd84e221efd3f23b1e53e2491
Andre Bras Correia <andre.bras-correia@siemens.com>
2022-05-13 17:01:30 +0100

    started profile for ca

---
+++ imports/ui/Pages/ProfileCA.jsx
@@ -0,0 +1,46 @@
+import React from 'react'; 
+import ReactDOM from "react-dom";
+import {Meteor} from "meteor/meteor"
+
+import "./styles.css";
+import SideBar from "./sidebar";
+import { Fragment } from "react/cjs/react.production.min";
+import { Indisponibilidades } from "./Indisponibilidades";
+import { Restricoes } from "./Restricoes";
+import { ConsultaPrivada } from "./ConsultaPrivada";
+
+
+
+
+export const ProfileCA = () => {
+
+    let user = Meteor.users.findOne({ id: Meteor.userId()});
+    console.log(user);
+    let username = user.username;
+            
+        return (
+            <Fragment>
+            <div id="profile">
+                <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+                <div  style={{position: "fixed", width: "80px", height: "50px", right: "120px", top: "36px"}}>
+                    <p id="div_username"></p>
+                    </div>
+                
+    
+                <div id="indisponibilidades" hidden>
+                <Indisponibilidades></Indisponibilidades>
+                </div>
+    
+                <div id="restricoes" hidden>
+                <Restricoes></Restricoes>
+                </div>
+    
+                <div id="consultaPrivada">
+                <ConsultaPrivada></ConsultaPrivada>
+    
+                <div id="page-wrap"></div>
+                </div>
+            </div>
+            </Fragment>
+        );
+};
