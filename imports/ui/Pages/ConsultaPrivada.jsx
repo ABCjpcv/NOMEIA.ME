324d4dd5832dc76e9cb7e89892953a01b170f823
Andre Bras Correia <andre.bras-correia@siemens.com>
2022-06-29 21:33:57 +0100

    trying to add selected column for referee to accept nomination

--- imports/ui/Pages/ConsultaPrivada.jsx
+++ imports/ui/Pages/ConsultaPrivada.jsx
@@ -5,57 +5,52 @@ import { userColumns } from "./userColumns";
 import { useTableSearch } from "./useTableSearch";
 import "antd/dist/antd.css";
 import { Meteor } from "meteor/meteor";
+import { useEffect } from "react/cjs/react.production.min";
 
-const fetchUsers = async () => {
+const fetchData = async () => {
   const { data } = await axios.get("Livro.json");
   return { data };
 };
 
-
-
 export function ConsultaPrivada() {
-
-  // STATES
-  const {selectedRowKeys, setSelectedRowKeys} = useState([]);
-
-  let selectRow = (record) => {
-    const selectedRowKeys = [...this.state.selectedRowKeys];
-    if (selectedRowKeys.indexOf(record.key) >= 0) {
-      selectedRowKeys.splice(selectedRowKeys.indexOf(record.key), 1);
-    } else {
-      selectedRowKeys.push(record.key);
-    }
-    setSelectedRowKeys({ selectedRowKeys });
-  };
-  let onSelectedRowKeysChange = (selectedRowKeys) => {
-    setSelectedRowKeys({ selectedRowKeys });
-  };
-
-
-
-
   let [searchVal] = useState(
-    (Meteor.user() == undefined) ? "" : Meteor.user().username
+    Meteor.user() == undefined ? "" : Meteor.user().username
   );
 
   const { filteredData, loading } = useTableSearch({
     searchVal,
-    retrieve: fetchUsers,
+    retrieve: fetchData,
   });
 
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: onSelectedRowKeysChange
-    };
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [loadingz, setLoading] = useState(false);
+
+  const start = () => {
+    setLoading(true); // ajax request after empty completing
+
+    setTimeout(() => {
+      setSelectedRowKeys([]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log("selectedRowKeys changed: ", selectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+  const hasSelected = selectedRowKeys.length > 0;
 
   return (
     <div
       className="demo-app"
       style={{ height: "10%", width: "auto", alignSelf: "center" }}
     >
-      <div className="demo-app-sidebar">
-        
-      </div>
+      <div className="demo-app-sidebar"></div>
       <div>
         <div className="demo-app-main" style={{ overflow: "auto" }}>
           <div className="container">
@@ -68,12 +63,17 @@ export function ConsultaPrivada() {
                 columns={userColumns}
                 loading={loading}
                 pagination={false}
-                onRow={(record) => ({
-                  onClick: () => {
-                    selectRow(record);
-                  }
-                })}
               />
+              <span
+                style={{
+                  marginLeft: 8,
+                }}
+              >
+                <br></br>
+                {hasSelected
+                  ? `Confirmar ${selectedRowKeys.length} jogos selecionados?`
+                  : "Nenhum jogo selecionado"}
+              </span>
             </div>
           </div>
         </div>
